1c4c781045eddb86e48cb6de6d13f6fcb449ee91
arcticlimer <vinigm.nho@gmail.com>
2020-11-27 13:58:36 -0300

    Change import ordering and listen port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,18 @@
 import express from 'express';
+import 'express-async-errors';
+
+import '@database/connection';
+
+import routes from './routes';
 import morgan from 'morgan';
-import 'express-async-errors'
-
-import './database/connection';
-
-import routes from './routes/routes';
-import errorHandler from './errors/handler';
+import errorHandler from '@errors/handler';
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('common'));
+
 app.use(routes);
 app.use(errorHandler);
 
-app.listen(3333);
+app.listen(8393);
