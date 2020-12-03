0c6d4d7d03c4fb1fc0e4bb51620dc3bf1730c0e2
arcticlimer <vinigm.nho@gmail.com>
2020-12-02 23:47:46 -0300

    Add component

---
+++ frontend/src/components/TopAppLogo.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as AppLogo } from '../assets/svg/appLogo.svg';
+
+function TopAppLogo() {
+  return (
+    <div id="logo-wrapper">
+    <Link to="/">
+      <AppLogo className="main-app-logo" />
+    </Link>
+  </div>
+  );
+}
+
+export default TopAppLogo;
