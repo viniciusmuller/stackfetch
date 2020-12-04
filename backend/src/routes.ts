903491de41995e4fd1695b0876a362cbe763257e
arcticlimer <vinigm.nho@gmail.com>
2020-12-03 23:49:54 -0300

    Create /technologies API route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import UserController from '@controllers/userController';
+import TechnologyController from '@controllers/technologyController';
 
 const routes = Router();
 
 routes.post('/users', UserController.create);
 routes.get('/users/:id', UserController.show);
 routes.get('/users', UserController.index);
+
+routes.get('/technologies', TechnologyController.index);
 
 export default routes;
