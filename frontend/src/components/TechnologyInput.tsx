e84e18c8fd420ba36a55ea20a866414dbe6e4901
arcticlimer <vinigm.nho@gmail.com>
2020-12-03 23:53:51 -0300

    Create component

---
+++ frontend/src/components/TechnologyInput.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TextField from '@material-ui/core/TextField';
+
+import { Technology } from '../common/types';
+import api from '../services/api';
+
+
+interface TechInputProps {
+  onChange: (e: React.ChangeEvent<{}>, values: Technology[]) => void;
+}
+
+function TechnologyInput(props: TechInputProps) {
+
+  const [technologies, setTechnologies] = useState<Technology[]>([]);
+
+  // Loading all available technologies on mount
+  useEffect(() => {
+    api.get('/technologies')
+      .then(res => setTechnologies(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <Autocomplete
+      multiple
+      autoHighlight
+      limitTags={4}
+      onChange={props.onChange}
+      getOptionLabel={(tech: Technology) => tech.name}
+      options={technologies}
+      noOptionsText="No technologies found."
+      renderInput={params => (
+        <TextField
+          {...params}
+          variant="standard"
+          label="Technologies"
+        />
+      )}
+    />
+  );
+}
+
+export default TechnologyInput;
