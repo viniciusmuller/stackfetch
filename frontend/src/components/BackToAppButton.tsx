5f7d9bda96f27a9baeadff40dfe418f631ee3922
arcticlimer <vinigm.nho@gmail.com>
2020-12-04 23:29:27 -0300

    Create component

---
+++ frontend/src/components/BackToAppButton.tsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+
+function BackToAppButton() {
+  return (
+    <Link to="/app" className="back-to-app">
+      <Button>
+        Back to the app
+      </Button>
+    </Link>
+  );
+}
+
+export default BackToAppButton;
