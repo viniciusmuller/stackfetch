be53eac70c2f3c679a0a8c6b93291d5a5eafc518
arcticlimer <vinigm.nho@gmail.com>
2020-12-07 22:10:42 -0300

    Create component

---
+++ frontend/src/components/LoadUsersButton.tsx
@@ -0,0 +1,18 @@
+import IconButton from '@material-ui/core/IconButton';
+import { FaPlusSquare } from 'react-icons/fa';
+
+function LoadUsersButton({ ...props }) {
+  return (
+    <>
+      <div className="load-users-break"></div>
+      <IconButton
+        className="load-users-button"
+        onClick={props.onClick}
+      >
+        <FaPlusSquare size="2.6rem" />
+      </IconButton>
+    </>
+  )
+}
+
+export default LoadUsersButton;
