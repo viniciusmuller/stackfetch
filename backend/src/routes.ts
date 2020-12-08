3bf9ff3dc9cc06ad7d32d2829fd21bc773dd7ca3
arcticlimer <vinigm.nho@gmail.com>
2020-12-07 22:08:23 -0300

    Versionate API and add new routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,14 +1,20 @@
 import { Router } from 'express';
 
+import { registerLimiter } from './rateLimits';
 import UserController from '@controllers/userController';
 import TechnologyController from '@controllers/technologyController';
 
 const routes = Router();
 
-routes.post('/users', UserController.create);
-routes.get('/users/:id', UserController.show);
-routes.get('/users', UserController.index);
+routes.post('/api/v1/users', registerLimiter, UserController.create);
+routes.get('/api/v1/users', UserController.index);
+routes.get('/api/v1/users/:id', UserController.show);
+routes.put('/api/v1/users/:id', UserController.edit);
+routes.delete('/api/v1/users/:id', UserController.destroy);
 
-routes.get('/technologies', TechnologyController.index);
+routes.get('/api/v1/technologies', TechnologyController.index);
+routes.post('/api/v1/technologies', TechnologyController.create);
+routes.put('/api/v1/technologies', TechnologyController.edit);
+routes.delete('/api/v1/technologies', TechnologyController.destroy);
 
 export default routes;
