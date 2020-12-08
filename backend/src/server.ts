3bb31aee58479d14d87c5de770d503d2aed08f20
arcticlimer <vinigm.nho@gmail.com>
2020-12-07 22:04:36 -0300

    Update server middlewares

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,22 +1,29 @@
 import express from 'express';
 import 'express-async-errors';
+import dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/.env' });
 import morgan from 'morgan';
 import cors from 'cors';
 
 // Connecting to the database
 import '@database/connection';
 
-import routes from './routes';
+import { apiLimiter } from './rateLimits';
+import { port } from '@config/config';
 import errorHandler from '@errors/handler';
+import routes from './routes';
+
+const middlewares = [
+  express.json(),
+  morgan('common'),
+  // TODO cors whitelist before deploy
+  cors(),
+  apiLimiter,
+  routes,
+  errorHandler,
+]
 
 const app = express();
+middlewares.forEach(m => app.use(m))
 
-app.use(express.json());
-app.use(morgan('common'));
-
-// TODO cors whitelist before deploy
-app.use(cors());
-app.use(routes);
-app.use(errorHandler);
-
-app.listen(8393);
+app.listen(port, () => console.log(`Server listening at ${port}!`));
