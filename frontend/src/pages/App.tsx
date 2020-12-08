1bb7250fd50b1258b414f2e3fc8b5d29d34f4b4f
arcticlimer <vinigm.nho@gmail.com>
2020-12-07 22:13:09 -0300

    Update page

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -7,6 +7,7 @@ import OptionsMenu from '../components/OptionsMenu';
 import SearchBar from '../components/SearchBar';
 import NoUsers from '../components/NoUsers';
 import UserCard from '../components/UserCard';
+import LoadUsersButton from '../components/LoadUsersButton';
 import Footer from '../components/Footer';
 import api from '../services/api';
 
@@ -15,9 +16,9 @@ function App() {
   const [userSearchValue, setUserSearchValue] = useState('');
   const [selectedTechnologies, setSelectedTechnologies] = useState<Technology[]>([]);
 
-  // Page content values
-  const [users, setUsers] = useState<User[]>();
+  const [users, setUsers] = useState<User[] | null>();
 
+  // Load our first page of users on component mount
   useEffect(() => {
     api.get('/users')
       .then(res => setUsers(res.data))
@@ -26,19 +27,24 @@ function App() {
 
   return (
     <>
-      <OptionsMenu onTechsChange={(_, techs) => setSelectedTechnologies(techs)}/>
+      <OptionsMenu
+        onTechsChange={(_, techs) => setSelectedTechnologies(techs)}
+      />
       <div id="app">
         <TopAppLogo />
         <SearchBar
           onClick={() => {
-            // TODO fetch API here
-            setTimeout(() => {
-              let search = {
-                username: userSearchValue,
-                stack: selectedTechnologies.map(t => t.id)
+            // Triggering loading animation
+            setUsers(null);
+            // Fetching users from the API and setting them
+            api.get('/users', {
+              params: {
+                name: userSearchValue,
+                stack: selectedTechnologies.map(t => t.id).join()
               }
-              console.log(search);
-            }, 1000);
+            })
+            .then(res => setUsers(res.data))
+            .catch(err => console.log(err));
           }}
           onChange={e => {
             setUserSearchValue((e.target as HTMLInputElement).value)
@@ -54,7 +60,25 @@ function App() {
             ? users.map(user => <UserCard key={user.id} {...user} />)
             : <NoUsers />)
           ) || <CircularProgress size={50} />}
-        </div>
+          {/* Check if the total users is divisible per 10 (page size).
+              If it is, we can try to load another page of users on the API */}
+          <div className="load-users-break"></div>
+          {(users && users.length % 10 === 0 && users.length > 0) && (
+            <LoadUsersButton
+              onClick={() => {
+                api.get('/users', {
+                  params: {
+                    name: userSearchValue,
+                    stack: selectedTechnologies.map(t => t.id).join(),
+                    page: users!.length / 10
+                  }
+                })
+                .then(res => setUsers(users!.concat(res.data)))
+                .catch(err => console.log(err));
+              }}
+            />
+          )}
+          </div>
         <Footer />
       </div>
     </>
--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,18 +1,28 @@
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 
+import TopAppLogo from '../components/TopAppLogo';
+import Footer from '../components/Footer';
+
 function NotFound() {
   return (
-    <div id="not-found">
-      <h1>That seems the wrong way...</h1>
-      <h2>Take this map and go back to our routes!</h2>
+    <>
+    <TopAppLogo />
+      <div id="not-found">
+        <h1>That seems the wrong way...</h1>
+        <h2>Take this map and go back to our routes!</h2>
 
-      <Link to="/app">
-        <Button>
-          Back to the routes
-        </Button>
-      </Link>
-    </div>
+        <Link
+          to="/app"
+          className="back-button"
+        >
+          <Button>
+            Back to the routes
+          </Button>
+        </Link>
+      <Footer />
+      </div>
+    </>
   );
 }
 
