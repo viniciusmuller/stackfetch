d99114b2583b918f3dd0ed44936beb20fb409277
arcticlimer <vinigm.nho@gmail.com>
2020-12-09 23:33:25 -0300

    Update migrations

---
+++ backend/src/database/migrations/1607531552691-populateTechnologies.ts
@@ -0,0 +1,28 @@
+import {MigrationInterface, QueryRunner} from 'typeorm';
+
+// technologies.json adapted from https://github.com/doda/github-language-colors
+import technologiesJSON from '../technologies.json';
+
+export class populateTechnologies1607531552691 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    for(const [tech, color] of Object.entries(technologiesJSON)) {
+      await queryRunner
+        .manager
+        .createQueryBuilder()
+        .insert()
+        .into('technologies')
+        .values({
+          name: tech,
+          color: color
+        })
+        .execute()
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner
+      .manager
+      .query('TRUNCATE TABLE technologies RESTART IDENTITY')
+  }
+}
