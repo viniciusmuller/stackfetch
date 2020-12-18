a92169ff927fc893adab075951db15d19426f266
arcticlimer <vinigm.nho@gmail.com>
2020-12-18 00:25:46 -0300

    Update app build file

--- build.sh
+++ build.sh
@@ -1,12 +1,2 @@
-c="docker-compose"  -f docker-compose.yml
-
-e=".env"
-ee=".env.example"
-# Creates a .env based on .env.example if .env doesn't exist
-[ -f $ee ] && [ ! -f $e ] && cp $ee $e
-[ $1 = "dev" ] || [ $1 = "prod" ] || [ $1 = "test" ] ||
-echo "usage: ./$0 <dev|prod|test>
-use -h or --help." && exit 1;
-
-$c -f $c.yml -f $c.$1.yml up
-
+(cd frontend && yarn && yarn build && mv build ../backend)
+(cd backend && yarn && yarn prod)
